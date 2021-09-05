766b9d54765696b7c390d1aade88784d3b46cda4
Rule2k <nicolaspaviot@gmail.com>
2021-09-05 17:24:06 +0200

    create a new function to getFoo, also a function to delay the execution of certains action

--- src/components/Foobartory/Foobartory.tsx
+++ src/components/Foobartory/Foobartory.tsx
@@ -10,12 +10,13 @@ enum Job {
 interface Robot {
   id: number;
   job: Job;
+  loading: boolean;
 }
 
 const Foobartory = () => {
   const startingRobotList = [
-    { id: 1, job: Job.Idle },
-    { id: 2, job: Job.Idle },
+    { id: 1, job: Job.Idle, loading: false },
+    { id: 2, job: Job.Idle, loading: false },
   ];
   const [robotList, setRobotList] =
     useState<ReadonlyArray<Robot>>(startingRobotList);
@@ -23,10 +24,32 @@ const Foobartory = () => {
   const [numberOfBar, setNumberOfBar] = useState<number>(0);
   const [numberOfFoobar, setNumberOfFoobar] = useState<number>(0);
 
+  const delay = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const getFoo = (id: number) => {
+    setRobotList((currentRobotList) =>
+      currentRobotList.map((robot) =>
+        robot.id === id
+          ? { ...robot, loading: true, job: Job.GettingFoo }
+          : robot
+      )
+    );
+    delay(1000).then(() => setNumberOfFoo((numberOfFoo) => numberOfFoo + 1));
+    setRobotList((currentRobotList) =>
+      currentRobotList.map((robot) =>
+        robot.id === id ? { ...robot, loading: false } : robot
+      )
+    );
+  };
+
   return (
     <div>
       {robotList.map(({ id }) => (
-        <div key={id}>Robot {id}</div>
+        <div key={id} onClick={() => getFoo(id)}>
+          Robot {id}
+        </div>
       ))}
       <Summary
         numberOfFoo={numberOfFoo}
