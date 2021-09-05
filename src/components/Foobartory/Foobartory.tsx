820e6188e09067ad237faeeac9ca8329ce41e772
Rule2k <nicolaspaviot@gmail.com>
2021-09-05 16:49:48 +0200

    creating the states to store the data of the app, Summary now display the current values of theses states

--- src/components/Foobartory/Foobartory.tsx
+++ src/components/Foobartory/Foobartory.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import Summary from "../Summary";
+enum Job {
+  GettingFoo,
+  GettingBar,
+  AssemblingFoobar,
+  AssemblingRobot,
+  Idle,
+}
+interface Robot {
+  id: number;
+  job: Job;
+}
 
 const Foobartory = () => {
-  return <div>Foobartory</div>;
+  const startingRobotList = [
+    { id: 1, job: Job.Idle },
+    { id: 2, job: Job.Idle },
+  ];
+  const [robotList, setRobotList] =
+    useState<ReadonlyArray<Robot>>(startingRobotList);
+  const [numberOfFoo, setNumberOfFoo] = useState<number>(0);
+  const [numberOfBar, setNumberOfBar] = useState<number>(0);
+  const [numberOfFoobar, setNumberOfFoobar] = useState<number>(0);
+
+  return (
+    <div>
+      {robotList.map(({ id }) => (
+        <div key={id}>Robot {id}</div>
+      ))}
+      <Summary
+        numberOfFoo={numberOfFoo}
+        numberOfBar={numberOfBar}
+        numberOfFoobar={numberOfFoobar}
+        numberOfRobot={robotList.length}
+      />
+    </div>
+  );
 };
 
 export default Foobartory;
--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-const Summary = () => {
-  return <div>Summary</div>;
+interface Props {
+  numberOfFoo: number;
+  numberOfBar: number;
+  numberOfFoobar: number;
+  numberOfRobot: number;
+}
+
+const Summary = ({
+  numberOfFoo,
+  numberOfBar,
+  numberOfFoobar,
+  numberOfRobot,
+}: Props) => {
+  return (
+    <div>
+      <span>Number of Foo : {numberOfFoo}</span>
+      <span>Number of Bar : {numberOfBar}</span>
+      <span>Number of Foobar : {numberOfFoobar}</span>
+      <span>Number of Robot : {numberOfRobot}</span>
+    </div>
+  );
 };
 
 export default Summary;
